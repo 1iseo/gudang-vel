9bd28a10f941e44a005cb78cb542dcc30a1b7e07
1iseo <96890370+1iseo@users.noreply.github.com>
2025-07-03 10:25:12 +0700

    initial barang impl

---
+++ database/migrations/2025_07_03_014508_create_barang_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('kode')->unique();
+            $table->string('nama');
+            $table->integer('stok')->default(0);
+            // TODO: Verify that storing images in fs is possible in a shared hosting environment
+            $table->string('image_path')->nullable(); // Optional field for storing image path
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BarangController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -12,9 +13,16 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
-Route::get('/barang', function () {
-    return Inertia::render('barang/index');
-})->name('barang.index');
+
+Route::controller(BarangController::class)->group(function() {
+    Route::get('/barang', 'index')->name('barang.index');
+    // Route::get('/barang/create', 'create')->name('barang.create');
+    // Route::post('/barang', 'store')->name('barang.store');
+    // Route::get('/barang/{barang}', 'show')->name('barang.show');
+    // Route::get('/barang/{barang}/edit', 'edit')->name('barang.edit');
+    // Route::put('/barang/{barang}', 'update')->name('barang.update');
+    // Route::delete('/barang/{barang}', 'destroy')->name('barang.destroy');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
