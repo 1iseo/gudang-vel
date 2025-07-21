742bb1b582cb98bae03cdcd5124a1b51868fe812
1iseo <96890370+1iseo@users.noreply.github.com>
2025-07-21 15:01:36 +0700

    rebuild again

---
+++ public/build/assets/input-error-DWBJE4R-.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CfqaGs3U.js";import{b as o}from"./button-Ce7q4PQb.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
