e3105230a6e8ad97b7f92505bfff99e8af5d6062
1iseo <96890370+1iseo@users.noreply.github.com>
2025-07-21 10:45:34 +0700

    add built files

---
+++ public/build/assets/input-error-CRxJOir6.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Dr_irZI3.js";import{b as o}from"./button-CQ_-L00I.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
