bb915f5546b7a5e0234da5f1add7599854525b8e
1iseo <96890370+1iseo@users.noreply.github.com>
2025-07-25 08:47:59 +0700

    add min stok col to barang table

---
+++ database/migrations/2025_07_25_014614_add_min_stok_to_barang.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('barang', function (Blueprint $table) {
+            // min_stok merupakan batas bawah stok barang sebelum dianggap hampir habis
+            // dan perlu ditampilkan di dashboard
+            $table->integer('min_stok')->default(0)->after('stok');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('barang', function (Blueprint $table) {
+            $table->dropColumn('min_stok');
+        });
+    }
+};
